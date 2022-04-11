1b503cb8af283cfc3878555e88cb01dd858d42b0
Jean Hoang <jeantknhoang@gmail.com>
2022-04-11 10:37:29 -0400

    Add sound to main menu

--- grannySmithWorkoutBuddy/View Controllers/ViewController.swift
+++ grannySmithWorkoutBuddy/View Controllers/ViewController.swift
@@ -6,13 +6,38 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var imageLayer:CALayer?
    
+    @IBOutlet var volSlider : UISlider!
+    var soundPlayer = AVAudioPlayer()
+    
+    @IBAction func volumeDidChange(sender : UISlider) {
+        soundPlayer.volume = volSlider.value
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let soundURL = Bundle.main.path(forResource: "backgroundmusic", ofType: "m4a")
+        let url = URL(fileURLWithPath: soundURL!)
+        
+        self.soundPlayer = try! AVAudioPlayer.init(contentsOf: url)
+        self.soundPlayer.currentTime = 0
+        self.soundPlayer.volume = volSlider.value
+        self.soundPlayer.numberOfLoops = -1
+        self.soundPlayer.play()
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        self.soundPlayer.stop()
+    }
     
     @IBAction func unwindToViewController (sender: UIStoryboardSegue) {}
+    
+
     
    
     override func viewDidLoad() {
